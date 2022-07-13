42a1c562cdd74d81c93acf161626e62d318f9bba
dom-elves <79201054+dom-elves@users.noreply.github.com>
2022-07-13 21:00:09 +0100

    just messing around for now, exploring the API structure
    stuff to do
    - use "web name" instead of "last name" as that caused a lot of problems
    - sort out table relationships
    - annoyingly, maybe stick with the naming convention of the API
    - probably some other stuff i haven't thought of yet

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -12,8 +12,18 @@ class APIController extends Controller
         $response = Http::get('https://fantasy.premierleague.com/api/bootstrap-static/');
       
         $decoded = json_decode($response->body());
-      
-        $data = $decoded->events;
-        dd($data);
+        $players = $decoded->elements;
+        // dd($players[330]);
+        // $data = $decoded->teams;
+
+        foreach ($players as $player) {
+            print nl2br($player->web_name . " " . $player->selected_by_percent . " " . "\n");
+        }
+
+        // foreach ($data as $team) {
+        //     print nl2br($team->name . " " . $team->strength_overall_home . " " . $team->strength_overall_away ."\n");
+   
+        // }
+        // dd($data);
     }
 }
