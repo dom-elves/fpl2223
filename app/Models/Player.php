53ebdcef00cd621852d7da0a4abd8f86576d6157
dom-elves <79201054+dom-elves@users.noreply.github.com>
2022-07-25 20:38:21 +0100

    [point/pop models] - updating while i remember
    can't really start writing any code around this until the season starts or gw1 finishes

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Team;
+use App\Models\PlayerPopularity;
+use App\Models\PlayersPointHistory;
 
 class Player extends Model
 {
@@ -14,6 +16,16 @@ class Player extends Model
     public function team()
     {   
         return $this->hasOne(Team::class, 'id', 'team_id'); //model, local key, foreign key (keys are relative to <-that model)
+    }
+
+    public function pointHistory()
+    {   
+        return $this->hasOne(PlayersPointHistory::class, 'player_id', 'player_id'); //model, local key, foreign key (keys are relative to <-that model)
+    }
+
+    public function popularity()
+    {   
+        return $this->hasOne(PlayerPopularity::class, 'player_id', 'player_id'); //model, local key, foreign key (keys are relative to <-that model)
     }
 
     
--- app/Models/PlayerPopularity.php
+++ app/Models/PlayerPopularity.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Player;
 
 class PlayerPopularity extends Model
 {
     use HasFactory;
+
+    public function players()
+    {
+        return $this->hasMany(Player::class, 'player_id', 'player_id');
+    }
 }
